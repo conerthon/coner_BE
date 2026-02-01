60001196e84d09361c1e404e62391444d1ed4490
soyee <hj37new@gmail.com>
2026-02-01 15:58:15 +0900

    여í여행 그룹 생성/ì가입 로직 수정

--- src/main/java/com/example/Traveler/domain/User.java
+++ src/main/java/com/example/Traveler/domain/User.java
@@ -9,11 +9,6 @@ import lombok.*;
 @Table(name = "users") // H2 DB 예약어와 충돌 방지
 public class User {
 
-    // 특정 ID만 가진 객체를 생성하기 위한 생성자
-    public User(Long id) {
-        this.id = id;
-    }
-
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
